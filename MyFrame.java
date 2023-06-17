53b0d008c0d414054198e6cf04b21efd6c5f1007
Thang Sian Kop <126444266+applesleaf@users.noreply.github.com>
2023-06-17 03:55:04 -0400

    Add files via upload

---
+++ MyFrame.java
@@ -0,0 +1,81 @@
+package clcok;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MyFrame extends JFrame {
+	
+	Calendar calender;
+	SimpleDateFormat timeFormat;
+	SimpleDateFormat dayFormat;
+	SimpleDateFormat dateFormat;
+	JLabel timeLabel;
+	JLabel dayLabel;
+	JLabel dateLabel;
+	String time;
+	String day;
+	String date;
+	
+	MyFrame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("My Clock");
+		this.setLayout(new FlowLayout());
+		this.setSize(350,200);
+		this.setResizable(true);
+		
+		timeFormat = new SimpleDateFormat("hh:mm:ss a");
+		dayFormat = new SimpleDateFormat("EEEE");
+		dateFormat = new SimpleDateFormat("MM/dd/ yyyy");
+		
+		timeLabel = new JLabel();
+		timeLabel.setFont(new Font("Verdana",Font.PLAIN,50));
+		timeLabel.setForeground(Color.red);
+		timeLabel.setOpaque(true);
+		
+		dayLabel = new JLabel();
+		dayLabel.setFont(new Font("Verdana",Font.PLAIN,35));
+		dayLabel.setForeground(Color.red);
+		dayLabel.setOpaque(true);
+		
+		
+		dateLabel = new JLabel();
+		dateLabel.setFont(new Font("Verdana",Font.PLAIN,35));
+		dateLabel.setForeground(Color.red);
+		dateLabel.setOpaque(true);
+		
+		
+		this.add(timeLabel);
+		this.add(dayLabel);
+		this.add(dateLabel);
+		this.setVisible(true);
+		
+		setTime();
+	}
+	
+	public void setTime() {
+		while(true) {
+			time = timeFormat.format(Calendar.getInstance().getTime());
+		    timeLabel.setText(time);
+		    
+			day = dayFormat.format(Calendar.getInstance().getTime());
+		    dayLabel.setText(day);
+		    
+		    date = dateFormat.format(Calendar.getInstance().getTime());
+		    dateLabel.setText(date);
+		    
+		    try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
